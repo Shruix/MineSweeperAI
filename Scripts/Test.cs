738e977f1e0df7aa89e610d43a17b352f9604b9d
Matthew <Matthewdev@outlook.com>
2020-10-14 19:10:54 +1000

    Initial commit

---
+++ Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperAI.Scripts {
+    class Test {
+        
+        public static void TestFunction() {
+            ChromeOptions options = new ChromeOptions();
+
+            ChromeDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("http://minesweeperonline.com/");
+        }
+    }
+
+    
+}
